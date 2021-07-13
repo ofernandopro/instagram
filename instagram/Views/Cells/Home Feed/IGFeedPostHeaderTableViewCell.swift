5b46d1d1b7380d5ef440734aa5061105986dee46
Fernando Moreira <fernandoepm.pro@gmail.com>
2021-07-13 18:34:16 -0300

    Creating feed post cell

---
+++ instagram/Views/Cells/Home Feed/IGFeedPostHeaderTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  IGFeedPostHeaderTableViewCell.swift
+//  instagram
+//
+//  Created by Fernando Moreira on 13/07/21.
+//  Copyright © 2021 Fernando Moreira. All rights reserved.
+//
+
+import UIKit
+
+class IGFeedPostHeaderTableViewCell: UITableViewCell {
+
+    
+
+}
